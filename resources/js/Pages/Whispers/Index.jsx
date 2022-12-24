308dbd585c62296cda0ba2159c6a816c1f1441ca
Claudio <claudio.dev29@gmail.com>
2022-12-25 00:52:16 +0100

    dark mode and user experience

--- resources/js/Pages/Whispers/Index.jsx
+++ resources/js/Pages/Whispers/Index.jsx
@@ -16,21 +16,26 @@ export default function Index({ auth, whispers }) {
     }
 
     return (
-        <AuthenticatedLayout auth={auth}>
+        <AuthenticatedLayout
+            auth={auth}
+            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Inicio</h2>}
+        >
             <Head title="Whispers" />
  
-            <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-                <form onSubmit={submit}>
-                    <textarea
-                        value={data.message}
-                        placeholder="¿En que estas pensando?"
-                        className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-                        onChange={e => setData('message', e.target.value)}
-                    ></textarea>
-                    <InputError message={errors.message} className="mt-2" />
-                    <PrimaryButton className="mt-4" processing={processing}>Whisper</PrimaryButton>
-                </form>
-                <div className="mt-6 bg-white shadow-sm rounded-lg divide-y">
+            <div className="max-w-2xl mx-auto p-3">
+                <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 mb-4 mt-4 shadow sm:rounded-lg">
+                    <form onSubmit={submit}>
+                        <textarea
+                            value={data.message}
+                            placeholder="¿En que estas pensando?"
+                            className="block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 dark:focus:ring-indigo-600 focus:ring-indigo-500 rounded-md shadow-sm"
+                            onChange={e => setData('message', e.target.value)}
+                        ></textarea>
+                        <InputError message={errors.message} className="mt-2" />
+                        <PrimaryButton className="mt-4" processing={processing}>Whisper</PrimaryButton>
+                    </form>
+                </div>
+                <div>
                     {whispers.map(whisper =>
                         <Whisper key={whisper.id} whisper={whisper} />
                     )}
